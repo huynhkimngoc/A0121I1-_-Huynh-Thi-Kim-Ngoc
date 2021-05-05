b9c53c90ec6dfff3f1abf53ca4a10cf256837a85
huynhkimngoc <huynhkimngoc198@gmail.com>
2021-05-05 20:22:29 +0700

    bai tap

--- A0121I1_Huynh_Thi_Kim_Ngoc_Module_2/src/Bai_5_access_modifier/Bai_tap/Circle.java
+++ A0121I1_Huynh_Thi_Kim_Ngoc_Module_2/src/Bai_5_access_modifier/Bai_tap/Circle.java
@@ -1,18 +1,22 @@
 package Bai_5_access_modifier.Bai_tap;
 
 public class Circle {
-    private double radius =1.0;
+    private double radius = 1.0;
     private String color = "red";
-    Circle(){
+
+    Circle() {
 
     }
-    Circle(double r){
+
+    Circle(double r) {
         this.radius = r;
     }
-    public double getRadius(){
+
+    public double getRadius() {
         return this.radius;
     }
-    public double getArea(){
-        return this.radius*this.radius*3.14;
+
+    public double getArea() {
+        return this.radius * this.radius * 3.14;
     }
 }
